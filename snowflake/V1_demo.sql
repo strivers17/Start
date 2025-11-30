a663ce2ff63745be10072a8bd748f4df7b934bba
strivers17 <rajeshsankars17@gmail.com>
2025-11-30 17:51:23 +0530

    Create V1_demo.sql

---
+++ snowflake/V1_demo.sql
@@ -0,0 +1,10 @@
+-- changeset STRIVER:create_table_1
+CREATE OR REPLACE TABLE public.employees_git (
+    employee_id   NUMBER NOT NULL,
+    first_name    STRING NOT NULL,
+    last_name     STRING,
+    email         STRING UNIQUE,
+    salary        NUMBER DEFAULT 50000,
+    hire_date     DATE DEFAULT CURRENT_DATE,
+    PRIMARY KEY (employee_id)
+);
